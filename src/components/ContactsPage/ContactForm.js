65c353d7b488670b8bd5ee3b829d0d9fce07ea30
Olind91 <111879063+Olind91@users.noreply.github.com>
2022-10-25 15:53:03 +0200

    troubleshooting onkeyup

--- src/components/ContactsPage/ContactForm.js
+++ src/components/ContactsPage/ContactForm.js
@@ -2,22 +2,30 @@ import React, {useState} from 'react'
 
 
 const ContactForm = () => {
-
+    
+    
     const [contactForm, setContactForm] = useState({name: '', email: '', comment: ''})
 
     const [formErrors, setFormErrors] = useState({})
 
     const [canSubmit, setCanSubmit] = useState(false)
 
+    
+
     const validate = (values) => {
+        
         const errors = {}
         const regex_email = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
+        const regex_name = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
+        
         if(!values.name)
             errors.name = "You must enter a name"
             if(values.name == "Hans") //Hehehehehe
                 errors.name ="You must enter a valid name"
-
+            else if(!regex_name.test(values.name))
+                errors.name = "Please spell your name correctly"
+        
+        
         if(!values.email)
             errors.email = "You must enter an e-mail address"
         else if (!regex_email.test(values.email))
@@ -29,33 +37,54 @@ const ContactForm = () => {
             errors.comment = "Your comment must be at least 5 characters"
 
         
-        if(Object.keys(errors).length === 0)
+            if(Object.keys(errors).length === 0 )
+            
             setCanSubmit(true)
+            
         else
             setCanSubmit(false)
 
-        
+            
         return errors;
+        
     }
+
+    
 
     const handleChange = (e) => {
         const {id, value} = e.target
         setContactForm({...contactForm, [id]: value})
+        
+       
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault()
         setFormErrors(validate(contactForm))
+        e.preventDefault()
     }
   
+    
+    
+    const handleKeyUp = (e) => {
+        e.preventDefault()
+    }
+    
+    
   
-
+    
+    
+    
+    
+    
+    
+    
+    
     return (
     
     <section className="contact-form">
         <div className="container">
             {
-            canSubmit ?
+            canSubmit  ?
             
             (<div className="d-flex">
                 <div>Thank you for your feedback! We probably wont be in touch since we have a lot of stuff to do.</div>
@@ -70,18 +99,18 @@ const ContactForm = () => {
                         <div className="inputs">
                             
                             <div className="names nameMail">
-                                <input id="name" type="text" placeholder="Your name" value={contactForm.name} onChange={handleChange}></input>
+                                <input id="name" type="text" placeholder="Your name" value={contactForm.name} onChange={handleChange} onKeyUp={handleKeyUp} required></input>
                                 <div className="errorMessage">{formErrors.name}</div>
                             </div>
 
                             <div className="mail nameMail">
-                                <input id="email" type="email" placeholder="Your mail" value={contactForm.email} onChange={handleChange}></input>
+                                <input id="email" type="email" placeholder="Your mail" value={contactForm.email} onChange={handleChange} onKeyUp={handleKeyUp} required></input>
                                 <div className="errorMessage">{formErrors.email}</div>
                             </div>
                         </div>
 
                         <div className="write">
-                            <textarea id="comment" placeholder="Comments" value={contactForm.comment} onChange={handleChange}></textarea>
+                            <textarea id="comment" placeholder="Comments" value={contactForm.comment} onChange={handleChange} onKeyUp={handleKeyUp} required></textarea>
                             <div className="errorMessage">{formErrors.comment}</div>
                         </div>
 
